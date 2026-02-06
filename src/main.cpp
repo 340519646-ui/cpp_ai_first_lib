feea627905e9960e6c77cd2138d304348ae15a0f
HJY <340519646-ui@users.noreply.github.com>
2026-02-06 10:46:44 +0800

    Add project target

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
+#include"math_utils.h"
 using namespace std;
 int main()
 {
-  cout<<"hello,world"<<endl;
-  cout<<2<<endl;
+  cout<<"add(2,3)="<<add(2,3)<<endl;
+  cout<<"mul(4,5)="<<mul(4,5)<<endl;
   return 0;
 }
